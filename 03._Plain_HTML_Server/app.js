e2aecfb3ebbe7b7a651e251bd6b6642aa7eb1a63
Anders Latif <andl@kea.dk>
2023-02-24 10:53:45 +0100

    Created the visitor count page

--- 03._Plain_HTML_Server/app.js
+++ 03._Plain_HTML_Server/app.js
@@ -3,6 +3,15 @@ const app = express();
 
 app.use(express.static("public"));
 
+const tanks = [
+    { name: "Leopard", nationality: "Germany" },
+    { name: "Tiger", nationality: "Germany", year: 1943 },
+    { name: "M1 Abrams", version: "M1" }
+];
+let visitorCount = 0;
+
+/* Pages */
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/frontpage/frontpage.html");
 });
@@ -10,6 +19,25 @@ app.get("/", (req, res) => {
 app.get("/tanks", (req, res) => {
     res.sendFile(__dirname + "/public/tanks/tanks.html");
 });
+
+app.get("/visitors", (req, res) => {
+    res.sendFile(__dirname + "/public/visitors/visitors.html");
+});
+
+/* API */
+
+app.get("/api/tanks", (req, res) => {
+    res.send({ data: tanks });
+});
+
+app.get("/api/visitors", (req, res) => {
+    res.send({ data: visitorCount });
+});
+
+app.put("/api/visitors", (req, res) => {
+    res.send({ data: ++visitorCount });
+});
+
 
 
 const PORT = 8080;
