b87d2e2f19f54bf7ac684d97a5b9e288aae4d77e
Anders Latif <andl@kea.dk>
2023-03-03 11:46:54 +0100

    Started serving the first pages

--- 04._Upper/app.js
+++ 04._Upper/app.js
@@ -1,10 +1,20 @@
 import express from "express";
 const app = express();
 
+import path from "path";
+
+app.use(express.static("public"));
+
 // import jokes from "./util/jokes.js";
 
 
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("public/pages/frontpage/frontpage.html"));
+});
 
+app.get("/IRLQuests", (req, res) => {
+    res.sendFile(path.resolve("public/pages/IRLQuests/IRLQuests.html"));
+});
 
 
 
