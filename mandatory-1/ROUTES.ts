b9fe6f6061bac3bf56dd5925a3f6a80b799015e6
bartbie <bartbie37@gmail.com>
2023-03-30 23:09:03 +0200

    feat(admin): added mock-up login page

--- mandatory-1/ROUTES.ts
+++
@@ -1,7 +0,0 @@
-export const SIDEBAR = [
-  { text: "Introduction", filename: "introduction" },
-  { text: "Express JS", filename: "" },
-  { text: "Astro", filename: "" },
-  { text: "SSR", filename: "" },
-  { text: "About", filename: "" },
-] as const;
