2299b994661c33fd14b88bd075afbf67d6b7b85b
bartbie <bartbie37@gmail.com>
2023-03-30 19:46:27 +0200

    build(node): made it possible to serve via express

---
+++ mandatory-1/ROUTES.ts
@@ -0,0 +1,7 @@
+export const SIDEBAR = [
+  { text: "Introduction", filename: "introduction" },
+  { text: "Express JS", filename: "" },
+  { text: "Astro", filename: "" },
+  { text: "SSR", filename: "" },
+  { text: "About", filename: "" },
+] as const;
---
+++ mandatory-1/server/app.mjs
@@ -0,0 +1,17 @@
+import express from 'express';
+import { handler as ssrHandler } from '../dist/server/entry.mjs';
+
+const app = express();
+const PORT = 3000;
+console.log('lol')
+
+app.use(express.static('dist/client/'))
+app.use(ssrHandler);
+
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!")
+  })
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
